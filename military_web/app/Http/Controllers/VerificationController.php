c577077731d77b408fa8dd2957fe04b0b195c46b
tri-white <olegovic101@gmail.com>
2023-11-02 18:05:54 +0200

    anti DDOS for requests for roles

--- military_web/app/Http/Controllers/VerificationController.php
+++ military_web/app/Http/Controllers/VerificationController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\RequestForRole;
-
+use Carbon\Carbon;
 class VerificationController extends Controller
 {
     public function verificationView(){
@@ -16,10 +16,17 @@ class VerificationController extends Controller
             return redirect()->route('welcome')->with('error', "Ваш запит на верифікацію вже отримано. Очікуйте результатів");
         }
 
-        $role_requests = RequestForRole::where('user_id', $userid)->where('approved', 'Відмовлено')->get();
-        if(!$role_requests->isEmpty()){
-            return redirect()->route('welcome')->with('error', "Ваш запит на верифікацію вже отримано. Очікуйте результатів");
+        $threshold = Carbon::now()->subHours(24);
+        
+        $role_requests = RequestForRole::where('user_id', $userid)
+            ->where('approved', 'Відмовлено')
+            ->where('created_at', '>=', $threshold)
+            ->get();
+        
+        if (!$role_requests->isEmpty()) {
+            return redirect()->route('welcome')->with('error', "Неможливо відправити запит. Повинно пройти 24 години з останньої спроби.");
         }
+        
 
         $request->validate([
             'verification_photo' => 'required|image|mimes:jpeg,png,jpg|max:2048',
