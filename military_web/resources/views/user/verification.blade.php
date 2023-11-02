f7072697b9c2bf5870a68c916e341ba0907dc9a4
tri-white <olegovic101@gmail.com>
2023-11-02 18:09:16 +0200

    bootstrap design added

--- military_web/resources/views/user/verification.blade.php
+++ military_web/resources/views/user/verification.blade.php
@@ -1,12 +1,17 @@
 @extends('shared/layout')
 
 @section('content')
-<main>
-    <form action="{{ route('verify', Auth::user()->id) }}" method="POST" enctype="multipart/form-data">
-        @csrf
-        <label for="verification_photo">Завантажте фотографію ваших документів, які підтверджують вашу особу як військовозобов'язаного</label>
-        <input type="file" id="verification_photo" name="verification_photo">
-        <button type="submit">Підтвердити</button>
-    </form>
+<main class="d-flex justify-content-center align-items-center" style="height: 75vh;">
+    <div class="col-md-6">
+        <form action="{{ route('verify', Auth::user()->id) }}" method="POST" enctype="multipart/form-data" class="p-3 bg-light border rounded">
+            @csrf
+            <h2 class="mb-4">Верифікація військових</h2>
+            <div class="mb-3">
+                <label for="verification_photo" class="form-label">Завантажте фотографію ваших документів, які підтверджують вашу особу як військовозобов'язаного</label>
+                <input type="file" id="verification_photo" name="verification_photo" class="form-control">
+            </div>
+            <button type="submit" class="btn btn-primary">Надіслати на обробку</button>
+        </form>
+    </div>
 </main>
 @endsection
