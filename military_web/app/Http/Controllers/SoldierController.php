a4af9023bc958e1b1dca7ae5beae8a3c9941b206
tri-white <olegovic101@gmail.com>
2023-11-03 20:25:22 +0200

    finished form with checks

--- military_web/app/Http/Controllers/SoldierController.php
+++ military_web/app/Http/Controllers/SoldierController.php
@@ -32,7 +32,7 @@ class SoldierController extends Controller
     
         $post->save();
     
-        return redirect()->route('welcome')->with('success', 'Успішно створено пост');
+        return redirect()->route('welcome')->with('success', 'Успішно створено оголошення');
     }
 
 
@@ -40,28 +40,27 @@ class SoldierController extends Controller
         return view('soldier/form_post-fundraising');
     }
 
-    public function create_postFundraising(Request $request, $userid){
+    public function create_postFundraising(Request $request, $userid)
+    {
         $request->validate([
-            'header' => 'required|string|max:255',
-            'content' => 'required|string',
-            'photo' => 'nullable|image|mimes:jpeg,jpg,png|max:2048',
+            'purpose' => 'required|string',
+            'goal_amount' => 'required|numeric|min:0',
+            'current_amount' => 'required|numeric|min:0',
             'category_id' => 'required|exists:category,id',
         ]);
-    
+
+        if($request->input('goal_amount') <= $request->input('current_amount')){
+            return redirect()->back()->with('error','Зібрана сума не може бути більшою або рівною за суму, яку потрібно зібрати. Уважно заповюйте поля у формі!');
+        }
         $post = new PostMoney();
         $post->user_id = $userid;
-        $post->header = $request->input('header');
-        $post->content = $request->input('content');
+        $post->purpose = $request->input('purpose');
+        $post->goal_amount = $request->input('goal_amount');
+        $post->current_amount = $request->input('current_amount');
         $post->category_id = $request->input('category_id');
-    
-        if ($request->hasFile('photo')) {
-            $photoPath = $request->file('photo')->store('public/postAsk');
-            $post->photo = $photoPath;
-        }
-    
         $post->save();
-    
-        return redirect()->route('welcome')->with('success', 'Успішно створено пост');
+
+        return redirect()->route('welcome')->with('success', 'Успішно створено оголошення про збір коштів');
     }
-    
+
 }
