c5a75c2eba9924218196809a2d1a10ae490afefb
tri-white <olegovic101@gmail.com>
2023-11-08 12:27:28 +0200

    added fields

---
+++ military_web/database/migrations/2023_11_08_102640_add_soldier_fields_to_users.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('military_rank')->nullable();
+            $table->string('composition')->nullable();
+            $table->text('profile')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('military_rank');
+            $table->dropColumn('composition');
+            $table->dropColumn('profile');
+        });
+    }
+};
