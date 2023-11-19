8aa9cd465fc00a8f7fca0aa39aad552e5891ebb9
tri-white <olegovic101@gmail.com>
2023-11-19 17:17:15 +0200

    styled main card in askPost detailed page

--- military_web/resources/views/detailed_pages/ask-post.blade.php
+++ military_web/resources/views/detailed_pages/ask-post.blade.php
@@ -1,18 +1,63 @@
 @extends('shared/layout')
 
 @section('content')
+@php
+    $propositionCount = App\Models\Proposition::where('post_ask_id', $postAsk->id)->count();
+    $user = App\Models\User::find($postAsk->user_id);
+    @endphp
 <div class="container my-5">
-    <div class="card mb-4">
-        <img src="{{ $postAsk->photo ? asset(str_replace('public/', 'storage/', $postAsk->photo)) : asset('no-image.jpg') }}" class="card-img-top" alt="Listing Photo">
-        <div class="card-body">
-            <h5 class="card-title">{{ $postAsk->header }}</h5>
-            <p class="card-text">{{ $postAsk->content }}</p>
-            @php
-            $category = App\Models\Category::where('id', $postAsk->category_id)->first();
-            @endphp
-            <p class="card-text">Category: {{ $category->name }}</p>
+<div class="row">
+        <div class="col-12">
+            <div class="card mb-4">
+                <div class="row g-0">
+                    <div class="col-3 p-3" style="height: 400px; width: 400px;">
+                        <img src="{{ $postAsk->photo ? asset(str_replace('public/', 'storage/', $postAsk->photo)) : asset('no-image.jpg') }}" class="card-img-top" alt="Listing Photo" style="width:100%; height:100%;">
+                    </div>
+                    <div class="col-8">
+                        <div class="card-body">
+                            <h5 class="mx-0 px-0 card-title" style="font-size:24px;">
+                                {{ $postAsk->header }}
+                            </h5>
+                            <pre class="card-text" style="font-size:20px; white-space: pre-wrap;">{{ $postAsk->content }}</pre>
+                            @php
+                                $category = App\Models\Category::where('id', $postAsk->category_id)->first();
+                            @endphp
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
+
+    <!-- Second Card (Takes Half Column) -->
+    <div class="row">
+        <div class="col-6 mb-0">
+            <div class="card mb-4 d-flex flex-column">
+                <div class="card-body flex-grow-1">
+                    <h5 class="card-title">Додаткова інформація</h5>
+                    <p class="card-text">Категорія: {{ $category->name }}</p>
+                    <p class="card-text">Дата публікації: {{ $postAsk->created_at }}</p>
+                    <p class="card-text">Останнє оновлення: {{ $postAsk->updated_at }}</p>
+                    <p class="card-text">Кількість пропозицій: {{ $propositionCount }}</p>
+                </div>
+            </div>
+        </div>
+
+        <!-- Third Card (Takes Half Column) -->
+        <div class="col-6">
+            <div class="card mb-4 d-flex flex-column">
+                <div class="card-body flex-grow-1">
+                    <h5 class="card-title">Автор запиту</h5>
+                    <p class="card-text">Імя: <a href="{{ route('profile', $user->id) }}">{{ $user->name }}</a></p>
+                    <p class="card-text">Ранг: {{ $user->military_rank }}</p>
+                    <p class="card-text">Склад: {{ $user->composition }}</p>
+                    <p class="card-text">Профіль: {{ $user->profile }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
 
     <!-- Form for making propositions -->
     <div class="card mb-4">
