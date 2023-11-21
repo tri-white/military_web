b19d8191213f7f1f875bb6d99ecdb560609b9439
tri-white <olegovic101@gmail.com>
2023-11-21 13:34:56 +0200

    added expiration scheduler command

---
+++ military_web/app/Console/Commands/ExpireAuctions.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class ExpireAuctions extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:expire-auctions';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
